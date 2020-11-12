18b8947511bc403e901a180069d4344dcb95a905
rtruong4 <51384814+rtruong4@users.noreply.github.com>
2020-11-12 15:23:35 -0800

    Update StudentAI.py

--- checkers-python/StudentAI.py
+++ checkers-python/StudentAI.py
@@ -5,7 +5,7 @@ from BoardClasses import Board
 #Students can modify anything except the class name and exisiting functions and varibles. hello
 #comment hello hello hello
 #this is test3
-#Check to see if downloaded
+#Check to see if downloaded 2
 class StudentAI():
 
     def __init__(self,col,row,p):
