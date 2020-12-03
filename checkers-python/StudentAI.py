6b346f69b22b771f08cfacf9af007c80da954d1d
Ryan Truong <rtruong63@gmail.com>
2020-12-02 21:19:44 -0800

    12/2/2020 9:19 pm  Changed a bit of code in rollout to optimize time (still not fast enough)

--- checkers-python/StudentAI.py
+++ checkers-python/StudentAI.py
@@ -5,6 +5,7 @@ import math
 import random
 import copy
 import time
+
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
 
@@ -39,11 +40,14 @@ class StudentAI():
   
     def mctSearch(self, root):
         currentTime = time.time()
+        counter = 0
+        countTime = 0
         while (time.time() - currentTime) < 15:
+            counter+= 1
             leaf = self.tree_policy(root)
             simResult = self.rollout(leaf)
             self.backpropogate(leaf, simResult)
-
+        raise ValueError(counter)
         return self.bestMove(root)
 
 
@@ -66,35 +70,43 @@ class StudentAI():
             for inner_index in range(len(allowedMoves[index])):
                 possibleMoves += 1
 
-        return len(node.visitedMoves) < possibleMoves
+        return len(node.children) < possibleMoves
 
 
-    def is_not_terminal(self, board, color):
-        return len(board.get_all_possible_moves(color)) > 0
-        #return board.is_win(color) == 0
+
 
 
     def tree_policy(self, node):
         currNode = node
         while self.is_not_terminal(currNode.board, currNode.color):
             if self.checkNotFullExpand(currNode):
-                allowedMoves = currNode.board.get_all_possible_moves(currNode.color)
+                # allowedMoves = currNode.board.get_all_possible_moves(currNode.color)
+                #
+                # listOfUnvisited = []
+                # for index in range(len(allowedMoves)):
+                #     for inner_index in range(len(allowedMoves[index])):
+                #         move = allowedMoves[index][inner_index]
+                #         if move not in currNode.visitedMoves:
+                #             listOfUnvisited.append(move)
+                #
+                # newMove = random.choice(listOfUnvisited)
+                # currNode.visitedMoves.append(newMove)
+                #
+                # bCopy = copy.deepcopy((currNode.board))
+                # bCopy.make_move(newMove, currNode.color)
+                # newNode = Node(bCopy, self.getOppositeColor(currNode.color), move = newMove, parent = currNode)
+                # currNode.addChild(newNode)
+                # return newNode
 
-                listOfUnvisited = []
-                for index in range(len(allowedMoves)):
-                    for inner_index in range(len(allowedMoves[index])):
-                        move = allowedMoves[index][inner_index]
-                        if move not in currNode.visitedMoves:
-                            listOfUnvisited.append(move)
-
-                newMove = random.choice(listOfUnvisited)
-                currNode.visitedMoves.append(newMove)
-
+                #allowedMoves = currNode.board.get_all_possible_moves(currNode.color)
+                newMoveIndex = randint(0, len(currNode.unvisitedMoves) - 1)
+                newMove = currNode.unvisitedMoves.pop(newMoveIndex)
                 bCopy = copy.deepcopy((currNode.board))
                 bCopy.make_move(newMove, currNode.color)
                 newNode = Node(bCopy, self.getOppositeColor(currNode.color), move = newMove, parent = currNode)
                 currNode.addChild(newNode)
                 return newNode
+
 
             else:
                 currNode = self.chooseBestChild(currNode)
@@ -109,7 +121,8 @@ class StudentAI():
 
 
 
-    def chooseBestChild(self, node, constant = 1.414):
+    # def chooseBestChild(self, node, constant = 1.414):
+    def chooseBestChild(self, node, constant=0.8):
         #Choose the best child based on UCB formula
         score = -1 #Keeps track of best UCB value
         bestChild = None  #List of nodes in case the UCB value is tied in different children
@@ -138,28 +151,29 @@ class StudentAI():
         else:
             raise ValueError
 
+    def is_not_terminal(self, board, color):
+        return len(board.get_all_possible_moves(color)) > 0
+
+
     def rollout(self, node):
         """From the given board, simulate a random game until win, loss, or tie and return the appropriate value"""
         boardCopy = copy.deepcopy(node.board)
         currColor = node.color
-        count = 0
-        while self.is_not_terminal(boardCopy, currColor):
-            count += 1
-            allowedMoves = boardCopy.get_all_possible_moves(currColor)
+        allowedMoves = boardCopy.get_all_possible_moves(currColor)
+        while len(allowedMoves) > 0:
+
+
 
             index = randint(0, len(allowedMoves) - 1)
             inner_index = randint(0, len(allowedMoves[index]) - 1)
             move = allowedMoves[index][inner_index]
 
 
-
             boardCopy.make_move(move, currColor)
-
-
 
             currColor = self.getOppositeColor(currColor)
 
-
+            allowedMoves = boardCopy.get_all_possible_moves(currColor)
         return boardCopy.is_win(self.getOppositeColor(currColor))
         # winner = boardCopy.is_win(self.getOppositeColor(currColor))
         # if winner == self.color:
@@ -184,35 +198,44 @@ class StudentAI():
 
 
         while node.parent is not None:
-
-            if node.color == self.color:
-            #if node.color == self.opponent[self.color]:
-            #If the color of the node is our color
-                if result == node.color:
-                    node.visits += 1
-                elif result == self.getOppositeColor(node.color):
-                    node.wins += 1
-                    node.visits += 1
-                elif result == -1:
-                    node.wins += 0.5
-                    node.visits += 1
-                else:
-                    raise ValueError
-            #elif node.color == self.color:
-            elif node.color == self.opponent[self.color]:
-            #If the color of the node is the opposite color
-                if result == node.color:
-                    node.visits += 1
-                elif result == self.getOppositeColor(node.color):
-                    node.wins += 1
-                    node.visits += 1
-                elif result == -1:
-                    node.wins += 0.5
-                    node.visits += 1
-                else:
-                    raise ValueError
+            # if node.color == self.color:
+            # #if node.color == self.opponent[self.color]:
+            # #If the color of the node is our color
+            #     if result == node.color:
+            #         node.visits += 1
+            #     elif result == self.getOppositeColor(node.color):
+            #         node.wins += 1
+            #         node.visits += 1
+            #     elif result == -1:
+            #         node.wins += 0.5
+            #         node.visits += 1
+            #     else:
+            #         raise ValueError
+            # #elif node.color == self.color:
+            # elif node.color == self.opponent[self.color]:
+            # #If the color of the node is the opposite color
+            #     if result == node.color:
+            #         node.visits += 1
+            #     elif result == self.getOppositeColor(node.color):
+            #         node.wins += 1
+            #         node.visits += 1
+            #     elif result == -1:
+            #         node.wins += 0.5
+            #         node.visits += 1
+            #     else:
+            #         raise ValueError
+            # else:
+            #     raise ValueError
+            if result == node.color:
+                node.visits += 1
+            elif result == self.getOppositeColor(node.color):
+                node.wins += 1
+                node.visits += 1
+            elif result == -1:
+                node.wins += 0.5
+                node.visits += 1
             else:
-                raise ValueError
+                 raise ValueError
             node = node.parent
         return
 
@@ -235,9 +258,16 @@ class Node():
         self.visits = 1
         self.wins = 0
         self.color = color
-        self.visitedMoves = [] #visitedMoves is a list of move objects
+        self.unvisitedMoves = self.flatten(board.get_all_possible_moves(color)) #unvisitedMoves is a list of move objects
+        #self.visitedMoves = []
 
 
+    def flatten(self, list):
+        flatList = []
+        for sub in list:
+            for item in sub:
+                flatList.append(item)
+        return flatList
 
     def addChild(self, node):
         #Adds a new child to this node
